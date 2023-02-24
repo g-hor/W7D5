74c7c78e1124f4a5b563185d66462208751e88d4
Gary <geehor@gmail.com>
2023-02-24 11:26:26 -0500

    Add sessionscontroller

--- reddit_on_rails/app/controllers/application_controller.rb
+++ reddit_on_rails/app/controllers/application_controller.rb
@@ -23,4 +23,7 @@ class ApplicationController < ActionController::Base
         redirect_to new_session_url unless logged_in?
     end
 
+    def ensure_logged_out
+        redirect_to users_url if logged_in?
+    end
 end
